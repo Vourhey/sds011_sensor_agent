d83194eeed3f7393f5abaf831cf90c49e64c4898
Vadim Manaenko <vadim.razorq@gmail.com>
2020-02-01 17:25:32 +0300

    Add test script for sensor

---
+++ test_sensor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/evn python3
+
+from sds011 import SDS011
+import time
+import sys
+
+if __name__ == "__main__":
+    port = '/dev/ttyUSB0'
+    if len(sys.argv) > 1:   # the second arg is treated as a port
+        port = sys.argv[1]
+
+    sensor = SDS011(port)
+
+    while True:
+        q = sensor.query()
+        t = time.localtime()
+        current_t = time.strftime("%H:%M:%S", t)
+        print("{}: {}".format(current_t, q))
+        time.sleep(25)
+
